13cde5414be8b3143918abe62a54d382f4b30a20
kehayov17 <radoi1708@gmail.com>
2025-05-22 19:24:32 +0300

    Create scheduler.tf

---
+++ terraform/scheduler.tf
@@ -0,0 +1,11 @@
+resource "google_cloud_scheduler_job" "trigger_api_pipeline" {
+  name        = "trigger-soccer-fixtures-pipeline"
+  description = "Triggers the Cloud Function via Pub/Sub every 3 hours"
+  schedule    = "0 */3 * * *"
+  time_zone   = "Etc/UTC"
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.trigger_topic.id
+    data       = base64encode("{}")
+  }
+}
