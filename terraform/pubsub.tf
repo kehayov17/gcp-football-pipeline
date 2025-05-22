7b81bbc3668bef79dda2588f145f2ce683577fba
kehayov17 <radoi1708@gmail.com>
2025-05-22 19:24:04 +0300

    Create pubsub.tf

---
+++ terraform/pubsub.tf
@@ -0,0 +1,16 @@
+resource "google_pubsub_topic" "trigger_topic" {
+  name = "scheduler-trigger"
+}
+
+resource "google_pubsub_topic" "output_topic" {
+  name = var.pubsub_output_topic
+}
+
+resource "google_pubsub_topic_iam_binding" "allow_scheduler_publish" {
+  topic = google_pubsub_topic.trigger_topic.name
+
+  role    = "roles/pubsub.publisher"
+  members = [
+    "serviceAccount:cloud-scheduler@system.gserviceaccount.com"
+  ]
+}
