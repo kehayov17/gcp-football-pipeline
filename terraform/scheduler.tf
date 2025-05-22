9b7ee69f01077d03e57798343f9562d6e91cbcf7
kehayov17 <radoi1708@gmail.com>
2025-05-22 19:46:13 +0300

    Update scheduler.tf

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_scheduler_job" "trigger_api_pipeline" {
-  name        = "trigger-soccer-fixtures-pipeline"
+  name        = "trigger-football-fixtures-pipeline"
   description = "Triggers the Cloud Function via Pub/Sub every 3 hours"
   schedule    = "0 */3 * * *"
   time_zone   = "Etc/UTC"
