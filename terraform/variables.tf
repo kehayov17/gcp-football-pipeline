5e931d496cb440c2f7e39d9378ac6d4fcca89709
kehayov17 <radoi1708@gmail.com>
2025-05-22 19:46:40 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,4 +3,4 @@ variable "region" {default="us-central1"}
 variable "function_source_dir" { default = "../cloud-functions/api-to-pubsub" }
 variable "dataflow_temp_location" {}
 variable "dataflow_staging_location" {}
-variable "pubsub_output_topic" { default = "soccer-api-topic" }
+variable "pubsub_output_topic" { default = "football-api-topic" }
