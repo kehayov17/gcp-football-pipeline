4d87c17388b8ba2e14619fc4aa4497761589f41d
kehayov17 <radoi1708@gmail.com>
2025-05-22 22:29:39 +0300

    Create apis.tf

---
+++ terraform/apis.tf
@@ -0,0 +1,39 @@
+resource "google_project_service" "cloud_functions" {
+  project = var.project_id
+  service = "cloudfunctions.googleapis.com"
+}
+
+resource "google_project_service" "cloud_scheduler" {
+  project = var.project_id
+  service = "cloudscheduler.googleapis.com"
+}
+
+resource "google_project_service" "pubsub" {
+  project = var.project_id
+  service = "pubsub.googleapis.com"
+}
+
+resource "google_project_service" "dataflow" {
+  project = var.project_id
+  service = "dataflow.googleapis.com"
+}
+
+resource "google_project_service" "storage" {
+  project = var.project_id
+  service = "storage.googleapis.com"
+}
+
+resource "google_project_service" "iam" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "cloud_run" {
+  project = var.project_id
+  service = "run.googleapis.com"
+}
+
+resource "google_project_service" "eventarc" {
+  project = var.project_id
+  service = "eventarc.googleapis.com"
+}
